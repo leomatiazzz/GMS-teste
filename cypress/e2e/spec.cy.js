c77f121b3d4b3e3860c67fbacf9b21a853d77fc2
leomatiazzz <leo21matias@hotmail.com>
2025-04-27 14:44:00 -0300

    Adicionando a automação da funcionalidade de  busca de filmes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,13 @@
 /// <reference types="cypress"/>
 
+/*
 describe("US-012-Funcionalidade: Cadastro de membros", () => {
   it("Deve fazer o cadastro de campos obrigatórios.", () => {
     cy.visit("http://127.0.0.1:8080/");
     cy.get("#signup-firstname").type("Léo");
     cy.get("#signup-lastname").type("Araújo");
-    cy.get("#signup-email").type("leo25matias@hotmail.com");
-    cy.get("#signup-phone").type("88973642379");
+    cy.get("#signup-email").type("leo26matias@hotmail.com");
+    cy.get("#signup-phone").type("88973642378");
     cy.get("#signup-password").type("Teste#2020");
     cy.get("#signup-button").click();
     cy.get("#signup-response").should(
@@ -15,3 +16,35 @@ describe("US-012-Funcionalidade: Cadastro de membros", () => {
     );
   });
 });
+*/
+
+describe("US-001 - Funcionalidade: Buscar filmes", () => {
+  it("Deve buscar um filme existente", () => {
+    cy.visit("http://127.0.0.1:8080/");
+
+    cy.get("#search-input").type("Mononoke");
+    cy.contains("Buscar").click();
+    cy.contains("Mononoke").should("be.visible");
+  });
+
+  /*  
+describe("US-001 - Funcionalidade: Buscar filmes", () => {  
+  it("Deve exibir mensagem de erro para filme inexistente", () => {
+    cy.visit("http://127.0.0.1:8080/");
+
+    cy.get("#search-input").type("FilmeQueNaoExiste123");
+
+    cy.contains("Buscar").click();
+
+    // Verifica se aparece uma mensagem do tipo "Filme não encontrado"
+    cy.contains("Nenhum filme encontrado").should("be.visible");
+  });
+
+  */
+});
+
+/*
+A automação de buscas de filmes funcionou bem. Para testar as automações,
+vá retirando o comentário de cada uma e comentando em bloco nas outras,
+para não causar nenhum problema no script do código.
+*/
